98a0140e095d64d6a19a41a5dfc24420e8e79568
Thanh Pham (SA) <v.thanhpt24@vinid.net>
2019-05-04 12:39:24 +0700

    Added bin/dev

---
+++ bin/dev/meta.js
@@ -0,0 +1,11 @@
+var META = {
+    url: 'http://localhost:3000/bin/dev',
+    modules: {
+        module1: {
+            url: 'http://localhost:3001/bin/dev'
+        },
+        module2: {
+            url: 'http://localhost:3002/bin/dev'
+        }
+    }
+};
