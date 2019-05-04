fa13cbe7ae050c1d5cbc3757b29c3dd28b89e1ae
Thanh Pham (SA) <v.thanhpt24@vinid.net>
2019-05-04 12:52:03 +0700

    base config

--- scripts/config.ts
+++ scripts/config.ts
@@ -9,8 +9,7 @@ export interface IAppConfig {
 
     /** Map all the service Url, if null the rootUrl will be used instead */
     services?: {
-        user: string;
-        subscription: string;
+        base: string;
     };
 
     /** Internal calculated: home + url */
@@ -21,5 +20,4 @@ export interface IAppConfig {
 if (!config.home) config.home = '/';
 if (!config.homeUrl) config.homeUrl = !config.home ? config.url : (config.url + config.home);
 if (!config.services) config.services = {} as any;
-if (!config.services.user) config.services.user = config.url;
-if (!config.services.subscription) config.services.subscription = config.url;
+if (!config.services.base) config.services.base = config.url;
